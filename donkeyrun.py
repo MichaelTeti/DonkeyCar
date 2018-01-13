586b0ed0adfc8d7d236eae6232c20536f1b74332
Michael A. Teti <mtetifau@gmail.com>
2018-01-13 20:06:50 +0000

    Add files via upload

--- donkeyrun.py
+++ donkeyrun.py
@@ -1,9 +1,9 @@
 #Define a vehicle to take and record pictures 10 times per second.
 
-from donkeycar import Vehicle
+from vehicle import Vehicle
 import os, sys
 from parts.camera import PiCamera
-from parts.datastore import Tub
+#from parts.datastore import Tub
 
 
 V = Vehicle()
@@ -13,8 +13,8 @@ cam = PiCamera()
 V.add(cam, outputs=['image'], threaded=True)
 
 #add tub part to record images
-tub = Tub(path='~/d2/gettings_started', 
-          inputs=['image'], 
+tub = Tub(path='~/d2/gettings_started',
+          inputs=['image'],
           types=['image_array'])
 V.add(tub, inputs=['image'])
 
